501a72335ec0a863bb2d54c59c440bdb91034b94
sskui12 <sskui1234@gmail.com>
2015-03-09 17:10:33 +0900

    commit

---
+++ src/First.java
@@ -0,0 +1,25 @@
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		adder(12,8);
+	}
+	
+	public static int adder(int val1, int val2)
+	{
+		int result;
+		int rest;
+		result = val1 + val2;
+		System.out.println(result);
+		result = val1 - val2;
+		System.out.println(result);
+		result = val1*val2;
+		System.out.println(result);
+		result = val1%val2;
+		rest = val1*val2 - val1*(val1%val2);
+		System.out.println(result + "and" + rest);
+		return 0;
+	}
+}
